3b75e22db892ed48712bdfa18a1e2216d1220842
Travis Veazey <travisvz@gmail.com>
2022-12-07 16:19:45 -0900

    refactor: rearrange internal structure, rename lib
    In preparation for generating a field of stars behind the nebula, we're
    moving the Nebula struct into a sub-module (and then re-exporting it),
    and will be adding a new sub-module to generate the star field.

---
+++ src/nebula.rs
@@ -0,0 +1,62 @@
+use rayon::prelude::*;
+use rand::prelude::*;
+use rand_xoshiro::Xoshiro256PlusPlus;
+
+const DEFAULT_SEED: u64 = 0xCAFEBABE;
+
+mod cloud;
+use cloud::GasCloud;
+use super::Color;
+
+pub struct Nebula {
+    size: usize,
+    scale: f32,
+    seed: u64,
+}
+
+impl Nebula {
+    pub fn new(size: usize) -> Self {
+        Self::seeded(size, DEFAULT_SEED)
+    }
+
+    pub fn seeded(size: usize, seed: u64) -> Self {
+        Self {
+            size,
+            scale: size as f32,
+            seed,
+        }
+    }
+
+    pub fn generate(self) -> Vec<u16> {
+        let mut rng = Xoshiro256PlusPlus::seed_from_u64(self.seed);
+
+        let clouds = vec![
+            GasCloud::new(Color::new(0.5, 1.0, 0.3, 1.0), rng.gen()),
+            GasCloud::new(Color::new(0.2, 1.0, 1.0, 0.8), rng.gen()),
+        ];
+
+        //image::Rgba([1409, 1409, 6291, u16::MAX])
+        let bg_color = Color::new(0.02, 0.02, 0.095, 1.0);
+
+        (0..(self.size.pow(2)))
+            .into_par_iter()
+            .map(|i| {
+                let (x, y) = self.get_xy(i);
+                // print!("\n{x},{y}: ");
+                clouds.iter()
+                    .map(|gas| gas.pixel(x, y))
+                    .fold(bg_color, |bg, fg| bg.blend(fg))
+                    .to_array()
+            })
+            .flatten()
+            .collect()
+    }
+
+    #[inline(always)]
+    fn get_xy(&self, i: usize) -> (f32, f32) {
+        let x = i % self.size;
+        let y = i / self.size;
+
+        (x as f32 / self.scale, y as f32 / self.scale)
+    }
+}
